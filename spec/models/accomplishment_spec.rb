38a112783c8b4361ef02761302d0e5f6a4b07fa7
Clint Mullins <clintfmullins@gmail.com>
2013-08-26 14:55:01 -0700

    accomplishment test

--- spec/models/accomplishment_spec.rb
+++ spec/models/accomplishment_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'factories'
 
 describe Accomplishment do 
 
@@ -8,15 +9,15 @@ describe Accomplishment do
 	it { should validate_presence_of :lesson }
 	it { should validate_presence_of :type }
 
-	# let(:accomplishment) { FactoryGirl.create(:accomplishment) }
+	let(:accomplishment) { FactoryGirl.create(:accomplishment) }
 
 	context '#create' do 
 		it 'should accept all necessary attributes upon creation' do
-			
+			expect(accomplishment).to be_a Accomplishment
 		end
 
 		it 'should have type of Accomplishment' do 
-
+			accomplishment.type.should eq 'Accomplishment'
 		end
 	end
 
