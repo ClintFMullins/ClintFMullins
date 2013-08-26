0174d6b629ba6549ae1c4299c62087035cffe55c
Clint Mullins <clintfmullins@gmail.com>
2013-08-26 14:58:04 -0700

    tests for hobbies

--- spec/models/hobbies_spec.rb
+++ spec/models/hobbies_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'factories'
 
 describe Hobby do 
 
@@ -7,5 +8,17 @@ describe Hobby do
 	it { should validate_presence_of :start_date }
 	it { should validate_presence_of :lesson }
 	it { should validate_presence_of :type }
+
+	let(:hobby) { FactoryGirl.create(:hobby) }
+
+	context '#create' do 
+		it 'should accept all necessary attributes upon creation' do
+			expect(hobby).to be_a Hobby
+		end
+
+		it 'should have type of Hobby' do 
+			hobby.type.should eq 'Hobby'
+		end
+	end
 
 end
