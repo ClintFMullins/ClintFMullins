58e2591751e17ded18e26872fe8049ef10832f31
Clint Mullins <clintfmullins@gmail.com>
2013-08-26 14:56:35 -0700

    employment tests passing

--- spec/models/employments_spec.rb
+++ spec/models/employments_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'factories'
 
 describe Employment do 
 
@@ -7,5 +8,17 @@ describe Employment do
 	it { should validate_presence_of :start_date }
 	it { should validate_presence_of :lesson }
 	it { should validate_presence_of :type }
+
+	let(:employment) { FactoryGirl.create(:employment) }
+
+	context '#create' do 
+		it 'should accept all necessary attributes upon creation' do
+			expect(employment).to be_a Employment
+		end
+
+		it 'should have type of Employment' do 
+			employment.type.should eq 'Employment'
+		end
+	end
 
 end
