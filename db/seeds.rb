526b3b6d631ddb50e635841a642a523b86cd4a4a
Clint Mullins <clintfmullins@gmail.com>
2013-08-27 09:05:39 -0700

    seeds are planted

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,29 @@ end
 								end_date:    time_rand,
 								lesson:      Faker::Lorem.sentence )
 end
+
+10.times do 
+	Employment.create( name: 			 Faker::Company.name,
+										 description: Faker::Lorem.sentence,
+										 start_date:  time_rand,
+										 end_date:    time_rand,
+										 lesson:      Faker::Lorem.sentence )
+end
+
+10.times do 
+	Hobby.create( name: 			 Faker::Company.name,
+								description: Faker::Lorem.sentence,
+								start_date:  time_rand,
+								end_date:    time_rand,
+								lesson:      Faker::Lorem.sentence )
+end
+
+10.times do 
+	Accomplishment.create( 
+		name: 			 Faker::Company.name,
+		description: Faker::Lorem.sentence,
+		start_date:  time_rand,
+		end_date:    time_rand,
+		lesson:      Faker::Lorem.sentence 
+	)
+end
