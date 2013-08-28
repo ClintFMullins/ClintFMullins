14fd41276fd8c579690d1ee17b637fff6f0f2218
Clint Mullins <clintfmullins@gmail.com>
2013-08-27 21:20:00 -0700

    initlizers doneso

--- app/assets/javascripts/experience.js
+++ app/assets/javascripts/experience.js
@@ -1,33 +1,49 @@
 $(document).ready(function(){
-  allExperiences = AllExperiences();
+  allExperiences = new AllExperiences();
 });
 
 
 function AllExperiences(){
   this.experiences = [];
-  $.each($('.experience'), function(i,experience){
-    console.log($(this));
-  });
+  this.init(this);
+}
+
+AllExperiences.prototype = {
+  init: function(self){
+    $.each($('.experience'), function(i,experience){
+      var name = $(this).find('.name').html();
+      var type = $(this).find('.type').html();
+      self.experiences.push(new Experience(name,type));
+    });
+  }
 }
 
 function Experience(name, type){
   this.name = name;
   this.type = type;
-  // this.view = new ExperienceView();
+  this.show = true;
+  this.view = new ExperienceView();
 }
 
-Experience.prototype = {
+Experience.prototype = {  
+  hide: function(){
+    this.show = false;
+    this.view.hide(this);
+  },
+
+  show: function(){
+    this.show = true;
+    this.view.show(this);
+  }
+}
+
+function ExperienceView(){
 
 
 }
 
-// function ExperienceView(){
+ExperienceView.prototype = {
 
 
-// }
-
-// ExperienceView.prototype = {
-
-
-// }
+}
 
