f844e0334adf5a4f82f1b14b4e391490247d58d8
Clint Mullins <clintfmullins@gmail.com>
2013-09-01 19:08:45 -0700

    tags on cards, ugly formatting

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,9 @@
 class Experience < ActiveRecord::Base
 	attr_accessible :name, :description, :start_date, :end_date, :lesson, :kind, :tags
 	validates_presence_of :name, :description, :start_date, :lesson, :kind
+
+	def nice_tags
+		tags
+	end
+
 end
